a2fed780185c850a51ccc2226a2b518ca68234b0
Airnov <124378651+Airnov@users.noreply.github.com>
2023-05-15 14:30:06 +0000

    comit

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,13 +1,40 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+import json
 
-answer = input("When was the first known use of the word 'monkey'? A: 1523 B: 1498 C: 1948 or D: 1043: ")
-if answer == "B":
-    print("Correct!")
-else:
-    print(f"The answer is 'B', not {answer!r}")
+def store_question(question, options, correct_answer):
+    with open("questions.txt", "w+") as f:
+        content = f.read()
+        CAdd = f"{question}\n{json.dumps(options)}\n{correct_answer}"
+        f.write(f"{content}{CAdd}\n")
 
-question = "When was the first known use of the word 'monkey'? A: 1523 B: 1498 C: 1948 or D: 1043"
+        """
+        f.write(f"{question}n")
+        f.write(f"{'n'.join(options)}n")
+        f.write(f"{correct_answer}n")
+        f.write("n")
+        """
 
-with open("questions.txt", "a") as f:
-    f.write(question + "\n")
+ques = ""
+ans = []
+cor = 0
+with open("questions.txt", "r") as f:
+    row = 0
+    for line in f.read().splitlines():
+        row += 1
+        if row == 1:
+            ques = line
+        elif row == 2:
+            ans = json.loads(line)
+        elif row == 3:
+            cor = int(line)
+
+question1 = input("Question: ")
+options1 = [input("1: "), input("2: "), input("3: "), input("4: ")]
+correct_answer1 = input("answer: ")
+store_question(question1, options1, correct_answer1)
+
+
+
+
+
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,30 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+import json
+
+ques = ""
+ans = []
+cor = 0
+with open("questions.txt", "r") as f:
+    row = 0
+    for line in f.read().splitlines():
+        row += 1
+        if row == 1:
+            ques = line
+        elif row == 2:
+            ans = json.loads(line)
+        elif row == 3:
+            cor = int(line)
+
+i = 0
+print(ques)
+for an in ans:
+    i+=1
+    print(f"{i}) {an}")
+input("Correct: ")
+
+user_answer = int(input("Enter the correct option number: "))
+if user_answer == cor:
+    print("Correct")
+
+
